8cb09a52576f7e014f316c6eccdac902840baad9
Georgiy95 <urijpetrovskij@gmail.com>
2023-07-03 19:03:02 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -15,8 +15,8 @@ const galleryCardsSet = createGallery(galleryItems);
 function createGallery(galleryItems) {
   return galleryItems
     .map(({ original, preview, description }) => {
-      return `<div class="gallery__item" style="border-radius: 4px; background: transparent; box-shadow: none;">
-  <a class="gallery__item" href="${original}" style= "box-shadow: none;" style="border-radius: 4px;">
+      return `<div class="gallery__item" style="background: transparent; box-shadow: none;">
+  <a class="gallery__item" href="${original}" style= "box-shadow: none;">
   <img class="gallery__image" src="${preview}" alt="${description}" style="border: 1px solid transparent;
    0px 2px 1px -1px rgb(0 0 0 / 12%);"/>
 </a>
