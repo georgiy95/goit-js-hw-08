ed5b5c9ff7f40c71c28924543808429ace37ea07
Georgiy95 <urijpetrovskij@gmail.com>
2023-07-03 20:52:38 +0300

    c

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,19 +1,19 @@
 import throttle from 'lodash.throttle';
 
-const LOCAL_KEY = 'feedback-form-state';
+const STORAGE_KEY = 'feedback-form-state';
 
-// form = document.querySelector('.feedback-form');
+const form = document.querySelector('.feedback-form');
 
 form.addEventListener('input', throttle(onInputData, 500));
 form.addEventListener('submit', onFormSubmit);
 
-let dataForm = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+let dataForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 const { email, message } = form.elements;
 reloadPage();
 
-function onInputData(e) {
+function onInputData(event) {
   dataForm = { email: email.value, message: message.value };
-  localStorage.setItem(LOCAL_KEY, JSON.stringify(dataForm));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
 }
 
 function reloadPage() {
@@ -23,15 +23,15 @@ function reloadPage() {
   }
 }
 
-function onFormSubmit(e) {
-  e.preventDefault();
+function onFormSubmit(event) {
+  event.preventDefault();
   console.log({ email: email.value, message: message.value });
 
   if (email.value === '' || message.value === '') {
-    return alert('Please fill in all the fields!');
+    return alert(`Будь ласка, заповніть всі обов'язкові поля.`);
   }
 
-  localStorage.removeItem(LOCAL_KEY);
-  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  event.currentTarget.reset();
   dataForm = {};
 }
